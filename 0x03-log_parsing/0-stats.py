c09ac4fc35505b79627c0631004d8cab7fd011af
Obada-Badee <obadabadee3@hotmail.com>
2024-07-19 03:57:42 +0300

    Update reading method

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,31 +7,32 @@ import re
 def print_info(total_size, code_dict):
     """Prints the desired infos """
 
-    print(f'File size: {total_size}', flush=True)
+    print(f'File size: {total_size}')
     for code, status in code_dict.items():
         if status > 0:
-            print(f"{code}: {status}", flush=True)
+            print(f"{code}: {status}",)
 
 
 def main():
     """Main entry point of the program"""
 
     total_size, counter = 0, 0
-    counter = 0
     code_dict = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
                  "404": 0, "405": 0, "500": 0}
     try:
-        for line in sys.stdin:
+
+        while True:
+            line = input()
             info_list = line.strip().split()
-            file_size = info_list[-1]
+            line_size = info_list[-1]
             current_code = info_list[-2]
 
             code_dict[current_code] += 1
-            total_size += int(file_size)
+            total_size += int(line_size)
             counter += 1
             if counter % 10 == 0:
                 print_info(total_size, code_dict)
-    except (KeyboardInterrupt, EOFError):
+    except (KeyboardInterrupt, EOFError) as ex:
         print_info(total_size, code_dict)
 
 
