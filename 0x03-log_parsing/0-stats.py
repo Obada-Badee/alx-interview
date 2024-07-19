c1aa28438597f4f5615e0e8342946abe2e1f14dd
Obada-Badee <obadabadee3@hotmail.com>
2024-07-19 04:58:34 +0300

    Add log validation

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 """ Log parsing Module """
-import sys
 import re
 
 
+def validate_line(line):
+    """
+    Validates if a log line matches the specified format.
+    """
+    pattern = (
+        r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.*\] '
+        r'"GET \/projects\/260 HTTP\/1\.1" \d{3} \d+$'
+    )
+    return re.match(pattern, line) is not None
+
+
 def print_info(total_size, code_dict):
-    """Prints the desired infos """
+    """
+    Prints the desired infos
+    """
 
     print(f'File size: {total_size}')
     for code, status in code_dict.items():
@@ -23,12 +35,12 @@ def main():
 
         while True:
             line = input()
-            info_list = line.strip().split()
-            line_size = info_list[-1]
-            current_code = info_list[-2]
-
-            code_dict[current_code] += 1
-            total_size += int(line_size)
+            if validate_line(line.strip()):
+                info_list = line.strip().split()
+                line_size = info_list[-1]
+                current_code = info_list[-2]
+                code_dict[current_code] += 1
+                total_size += int(line_size)
             counter += 1
             if counter % 10 == 0:
                 print_info(total_size, code_dict)
